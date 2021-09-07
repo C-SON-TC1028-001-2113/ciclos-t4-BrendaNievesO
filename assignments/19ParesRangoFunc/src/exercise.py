92fb2754258e3141bef3fd19a97e7ce42052b133
BrendaNievesO <a01252478@itesm.mx>
2021-09-07 01:23:38 +0000

    T4

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,5 +1,22 @@
 
 def main():
+    num1 = int(input("Valor 1: "))
+    num2 = int(input("Valor 2: "))
+
+    v1=num1
+    v2=num2
+
+    if v1>0 and v2>0:
+        if v2>v1:
+                for valor in range(v1,v2+1):
+                    if valor%2==0:
+                        print(valor)
+        elif v1>v2:
+                for valor in range(v2,v1+1):
+                    if valor%2==0:
+                        print(valor)
+    else:
+            print("No hay pares")     
 
 
 if __name__=='__main__':
--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,6 +1,11 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    height = int(input("Enter triangle height: "))
+    if height > 0:
+        for p in range(1, height+1):
+            print(' '*(height-p)+ '*'*p)
+
 
 
 if __name__=='__main__':
